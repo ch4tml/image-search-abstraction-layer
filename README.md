82246457d5bde2586702358d29511b8b11c48db8
Tom <t.m.lemanski@googlemail.com>
2016-02-05 15:54:50 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Image Search Abstraction Layer
+
+Built as part of Free Code Camp's curriculum. API Challenge series - [Image Search Abstraction Layer] (http://www.freecodecamp.com/challenges/image-search-abstraction-layer).
+
+Based on the example service [provided] (https://cryptic-ridge-9197.herokuapp.com/).
+
+### Heroku deployment: [Image Search Abstraction Layer] (https://ch4tml-is-al.herokuapp.com/)
+
+## Author 
+
+Built by [ch4tml](https://github.com/ch4tml/). Started on 5th February 2016 and finished on xx February 2016.
---
+++ app.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var express = require("express");
+var dotenv = require('dotenv').config();
+var app = express();
+var routes = require("./routes/routes");
+var bodyParser = require('body-parser');
+var path = require("path");
+var http = require("http");
+
+// Database setup
+var mongo = require("mongodb").MongoClient;
+var mongoose = require("mongoose");
+var configDB = process.env.MONGOLAB_URI;
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// All environments
+app.set('port', process.env.PORT || 8080);
+app.use(express.favicon());
+// Logs actions and requests when server running
+app.use(express.logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get("/", routes.index); // Home route
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
